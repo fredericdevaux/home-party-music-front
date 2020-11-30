0609af456d45395124ca08976f52c6b33367f47b
Frédéric Devaux <frederic@fcinq.com>
2020-11-30 20:22:32 +0100

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '@/plugins/colyseus.js', ssr: false }],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
